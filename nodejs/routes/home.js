6eb2770e0e0021070a9998bb7618f9bb94a8ad1e
e8517027 <541782164@qq.com>
2014-11-07 18:02:58 +0800

    edit

---
+++ nodejs/routes/home.js
@@ -0,0 +1,7 @@
+﻿exports.home = function(req, res){
+var user={
+username:'admin',
+password:'admin'
+}
+res.render('home', { title: 'Home',user: user});
+};
---
+++ nodejs/routes/login.js
@@ -0,0 +1,17 @@
+﻿exports.login = function(req, res){
+res.render('login', { title: '用户登陆'});
+};
+exports.doLogin = function(req, res){
+var user={
+username:'admin',
+password:'admin'
+}
+if(req.body.username===user.username && req.body.password===user.password){
+res.redirect('/home');
+}
+
+res.redirect('/login');
+};
+exports.logout = function(req, res){
+res.redirect('/');
+};
