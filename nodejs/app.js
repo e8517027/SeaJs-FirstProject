0c472ea89382c78aee06594dde29ad6a694fbc9f
e8517027 <541782164@qq.com>
2014-11-07 17:51:05 +0800

    nodejs

--- nodejs/app.js
+++ nodejs/app.js
@@ -6,15 +6,18 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var login=require('./routes/login');
+var home=require('./routes/home');
 var http = require('http');
 var path = require('path');
-
+var ejs = require('ejs');
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -30,7 +33,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/login', login.login);
+app.post('/login', login.doLogin);
+app.get('/logout', login.logout);
+app.get('/home', home.home);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
